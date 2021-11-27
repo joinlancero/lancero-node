136625253758e2e3fad298f0b6b82d5d9790c95b
Dries Augustyns <augustyns.dries@gmail.com>
2021-11-27 13:29:22 +0100

    changes

--- __tests__/connection.ts
+++ __tests__/connection.ts
@@ -1,12 +1,10 @@
 import { Lancero } from "../src";
 
 test("Making a connection", async () => {
-  const lancero = new Lancero(
-    "cc355ba5fc5b5527b87d7ef211c5cf6845c2443846390cb4"
-  );
+  const lancero = new Lancero("");
 
   // Generating codes
-  //const result = await lancero.codes.generate({ amount: 1 });
+  //const result = await lancero.codes.generate({ amount: 1, allowedClaims: 2 });
 
   // Searching for a code
   //const code = await lancero.codes.find("ultimate-iron");
@@ -18,12 +16,13 @@ test("Making a connection", async () => {
   //const searched_customer = await lancero.customers.find("hello@lancero.app");
 
   // Creating a customer
-  const created_customer = await lancero.customers.create({
-    email: "inbox@driaug.com",
-  });
+  //const created_customer = await lancero.customers.create({
+  //  email: "inbox@driaug.com",
+  //  waitlist: true,
+  //});
 
   // Deleting a customer
-  //const deleted_customer = await lancero.customers.delete('inbox@driaug.com')
+  //const deleted_customer = await lancero.customers.delete("inbox@driaug.com");
 
   // Deleting a code
   //const deleted_code = await lancero.codes.delete("elastic-code");
